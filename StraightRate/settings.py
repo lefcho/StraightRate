725e1273582ac23dc3ec60def55e6cc1abaffe53
ivand <ivan.d.kostov@gmail.com>
2024-10-02 19:25:08 +0300

    added a functional 404 page

--- StraightRate/settings.py
+++ StraightRate/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d4iaadvlj8fv_ci0vs(lsk^)w^s3bkibdng#i0el3=-2!savn5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
